b1bd2c152ad908d131500220fb27d05804581b08
Anders Pitman <tapitman11@gmail.com>
2015-05-08 01:56:41 -0700

    Working And, Or, BUffer, and Not
    Simple link architecture. Seems pretty good for a proof of
    concept. Changes to state are propagated (no pun intended)
    downstream via callbacks.

--- gates.py
+++ gates.py
@@ -1,14 +1,81 @@
 
-class And(object):
+class Link(object):
     def __init__(self):
-        self._in1 = False
-        self._in2 = False
+        self._state = False
+        self._callback = None
 
-    def evaluate(self):
-        return self._in1 and self._in2
+    def get_state(self):
+        return self._state
 
+    def set_state(self, state):
+        self._state = state
+        if self._callback:
+            self._callback(self._state)
+
+    def set_callback(self, callback):
+        self._callback = callback
+
+
+class Gate(object):
+    def _callback(self, state):
+        pass
+
+
+class OneInputGate(Gate):
+    def __init__(self, newIn=None, out=None):
+        self._in = newIn if newIn is not None else Link()
+        self._in.set_callback(self._callback)
+        self._out = out if out is not None else Link()
+
+
+    def get_in(self):
+        return self._in
+
+    def get_out(self):
+        return self._out
+
+
+class Buffer(OneInputGate):
+    def _callback(self, state):
+        self._out.set_state(self._in.get_state())
+
+
+class Not(OneInputGate):
+    def _callback(self, state):
+        self.get_out().set_state(not self.get_in().get_state())
+
+
+class TwoInputGate(Gate):
+    def __init__(self, in1=None, in2=None, out=None):
+        self._in1 = in1 if in1 is not None else Link()
+        self._in2 = in2 if in2 is not None else Link()
+        self._out = out if out is not None else Link()
+        self._in1.set_callback(self._callback)
+        self._in2.set_callback(self._callback)
+
+    def get_out(self):
+        return self._out
+
+    def get_in1(self):
+        return self._in1
+        
     def set_in1(self, in1):
         self._in1 = in1
 
+    def get_in2(self):
+        return self._in2
+
     def set_in2(self, in2):
         self._in2 = in2
+
+
+class Or(TwoInputGate):
+    def _callback(self, state):
+        self._out.set_state(self.get_in1().get_state() or 
+                            self.get_in2().get_state())
+
+
+class And(TwoInputGate):
+    def _callback(self, state):
+        self._out.set_state(self._in1.get_state() and self._in2.get_state())
+
--- tests.py
+++ tests.py
@@ -1,19 +1,91 @@
 
 from unittest import TestCase, main
 
-from gates import And
+from gates import *
+
+
+class TestBuffer(TestCase):
+
+    def test_cascade(self):
+        buf1 = Buffer()
+        buf2 = Buffer(buf1.get_out())
+        buf3 = Buffer(buf2.get_out())
+        self.assertFalse(buf3.get_out().get_state())
+        buf1.get_in().set_state(True)
+        self.assertTrue(buf3.get_out().get_state())
+
+
+class TestNot(TestCase):
+
+    def test_not(self):
+        not0 = Not()
+        not1 = Not(not0.get_out())
+        not0.get_in().set_state(True)
+        self.assertFalse(not0.get_out().get_state())
+        self.assertTrue(not1.get_out().get_state())
+        not0.get_in().set_state(False)
+        self.assertTrue(not0.get_out().get_state())
+
+
+class TestAnd(TestCase):
+    def setUp(self):
+        pass
+
+    def test_and(self):
+        and_gate = And()
+        and_gate.get_in1().set_state(True)
+        and_gate.get_in2().set_state(True)
+        self.assertTrue(and_gate.get_out().get_state())
+        and_gate.get_out().set_state(False)
+        self.assertFalse(and_gate.get_out().get_state())
+
+    def test_cascade(self):
+        and0 = And()
+        and1 = And()
+        and2 = And(and0.get_out(), and1.get_out(), Link())
+        self.assertFalse(and2.get_out().get_state())
+        and0.get_in1().set_state(True)
+        self.assertFalse(and2.get_out().get_state())
+        and0.get_in2().set_state(True)
+        self.assertFalse(and2.get_out().get_state())
+        and1.get_in1().set_state(True)
+        self.assertFalse(and2.get_out().get_state())
+        and1.get_in2().set_state(True)
+        # finally true
+        self.assertTrue(and2.get_out().get_state())
+
+
+class TestOr(TestCase):
+    def test_or(self):
+        gate = Or()
+        self.assertFalse(gate.get_out().get_state())
+        gate.get_in1().set_state(True)
+        self.assertTrue(gate.get_out().get_state())
+        gate.get_in1().set_state(False)
+        self.assertFalse(gate.get_out().get_state())
+
+
 
 class Tests(TestCase):
 
     def setUp(self):
-        self.and_gate = And()
+        self.link = Link()
 
-    def test_and(self):
-        self.and_gate.set_in1(True)
-        self.and_gate.set_in2(True)
-        self.assertTrue(self.and_gate.evaluate());
-        self.and_gate.set_in2(False)
-        self.assertFalse(self.and_gate.evaluate());
+
+    def test_link(self):
+        self.assertFalse(self.link.get_state())
+        callback_data = {}
+        def callback(state):
+            callback_data['called'] = True
+            callback_data['state'] = state
+        self.link.set_callback(callback)
+        self.link.set_state(True)
+        self.assertTrue(self.link.get_state())
+        self.assertTrue(callback_data['called'])
+        self.assertTrue(callback_data['state'])
+        self.link.set_state(False)
+        self.assertFalse(callback_data['state'])
+
 
 if __name__ == '__main__':
     main()
